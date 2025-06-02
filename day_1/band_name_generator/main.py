3c78d7d59f5d49b12a00cf642571000b31cb83cf
ayush <ayush16.mitmpl2023@learner.manipal.edu>
2025-06-02 06:57:11 +0530

    This is my first day of learnig python where I learned print and input functions and commneting and writing basic python program

---
+++ day_1/band_name_generator/main.py
@@ -0,0 +1,8 @@
+'''
+    This program uses basic print and input function to take data from the user and print it in the output console by concatenating the string and variables and producing the band name generator
+'''
+print("Welcome to the Band Name Generator")
+city_name = input("What is the name of the city you grew up in?\n")
+pet_name = input("What is the name of your pet\n")
+print("Your band name is " + city_name + " " + pet_name)
+
---
+++ day_1/hello_world/hello_world.py
@@ -0,0 +1,5 @@
+'''
+This is my first basicpython program, that people ususally write when they start a programming language, i.e., the "Hello World", which is just printed onto the output display.
+'''
+print("Hello World")
+
