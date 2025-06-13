419a515b6cd5c8e601fbfb04c05287f28b0c489e
ayush <ayush16.mitmpl2023@learner.manipal.edu>
2025-06-13 10:11:16 +0530

    This program uses basic data type conversion and mathematical operators to calculate the price that each person has to pay in a group based on the total bill the received and the tip they decided to pay

---
+++ day_2/tip_calculator/main.py
@@ -0,0 +1,10 @@
+#this program is used to calculate tip for the bill received
+print("Welcome to tip calculator!")
+bill = float(input("What is the total bill? Rs."))
+tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
+no_of_people = int(input("How many people to split the bill? "))
+bill += bill*tip/100
+per_head_cost = bill/no_of_people
+
+print(f"Each person has to pay Rs.{round(per_head_cost, 2)}")
+
