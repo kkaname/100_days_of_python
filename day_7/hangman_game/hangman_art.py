cf5378c6891fcea26cc894cb260a2ba252e2fd64
ayush <ayushhegde2005@gmail.com>
2025-08-04 23:07:16 +0530

    this module contains all the ascii art that will appear in the game

---
+++ day_7/hangman_game/hangman_art.py
@@ -0,0 +1,70 @@
+#this module contails all the ascii art the will be imported to the main.py file
+#r""" --> means raw string, i.e., it tells python not to see blackslashs as escape sequence
+#this below here, is called 'manual indexing using variables'
+life = {
+    1: r"""   +---+
+   |   |
+   O   |
+       |
+       |
+       |
+ =========""",
+
+    2: r"""   +---+
+   |   |
+   O   |
+   |   |
+       |
+       |
+ =========""",
+
+    3: r"""   +---+
+   |   |
+   O   |
+  /|   |
+       |
+       |
+ =========""",
+
+    4: r"""   +---+
+   |   |
+   O   |
+  /|\  |
+       |
+       |
+ =========""",
+
+    5: r"""   +---+
+   |   |
+   O   |
+  /|\  |
+  /    |
+       |
+ =========""",
+
+    6: r"""   +---+
+   |   |
+   O   |
+  /|\  |
+  / \  |
+       |
+ ========="""
+}
+
+logo =  '''Welcome to, 
+ _                                                
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ ' / _` | '_ ' / _` | '_ ` _ ' / _` | '_ ' 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|'__,_|_| |_|'__, |_| |_| |_|'__,_|_| |_|  GAME
+                    __/ |                      
+                   |___/    
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
+'''
