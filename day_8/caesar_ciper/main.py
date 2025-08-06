714fe5419767642fb66737f8576cb5871cf89c11
ayush <ayushhegde2005@gmail.com>
2025-08-07 01:38:49 +0530

    instead of having two different funtion, each for encryption and decryption, i have clubed them into one single funtion named 'caesar', which produces the same output as the two functions before

--- day_8/caesar_ciper/main.py
+++ day_8/caesar_ciper/main.py
@@ -6,31 +6,21 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def encryption(text):
-    encrypted_text = []
-    for word in text:
+def caesar(original_text, shift_number, encode_or_decode):
+    output_text = ""
+    if encode_or_decode == "decode":
+        shift_number *= -1
+
+    for word in original_text:
         if(word == " "):
-            encrypted_text += " "
+            output_text += " "
             continue
-        encrypted_text += alphabet[alphabet.index(word) + shift]
 
-    print("Here's the encoded result: ", end='')
-    for word in encrypted_text:
-        print(word, end='')
-    print("\n")
+        shifted_position = alphabet.index(word) + shift_number
+        shifted_position %= len(alphabet)
+        output_text +=  alphabet[shifted_position]
 
-def decryption(encrypted_text):
-    decrypted_text = []
-    for word in encrypted_text:
-        if(word == " "):
-            decrypted_text += " "
-            continue 
-        decrypted_text  += alphabet[alphabet.index(word) - shift]
-
-    print("Here's the decoded result: ", end='')
-    for word in decrypted_text:
-        print(word, end='')
-    print("\n")
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
 
     
 option = "yes"
@@ -38,13 +28,11 @@ while(option == "yes"):
     direction = input("Type 'encode' to encrypt or type 'decode' to decrypt: ").lower()
     text = input("Type you message: \n").lower()
     shift =  int(input("Type the shift number: "))
-    #
-    if (direction == "encode"):
-        encryption(text)
-    elif (direction == "decode"):
-        decryption(text)
+    if (direction == "encode" or direction == "decode"):
+        caesar(text, shift, direction)
     else:
         print("You have entered the wrong option, please try again.")
     #ask the user that does they want to encode/decode again
     option = input("Type 'yes' if you want to go again.x Otherwise type 'no': ").lower()
+    print("\n")
 
